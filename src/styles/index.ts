1fa540e16becbf4caefb7d5655b71a590b3a92da
Daniel Persson <persson.daniel.1990@gmail.com>
2021-04-06 19:11:42 +0200

    Add styles back

---
+++ src/styles/index.ts
@@ -0,0 +1,14 @@
+/**
+ * Merging theme.
+ *
+ * @author Daniel Persson
+ * @version 1.0.0
+ */
+
+ import { theme } from "./Theme";
+ import { overrides } from "./Overrides";
+ 
+ export const mainTheme = Object.assign(theme, overrides);
+ 
+ export type MainTheme = typeof mainTheme;
+ 

3924045fc9e35de34c0f4430d039f96a9d9b19fb
Daniel Persson <persson.daniel.1990@gmail.com>
2021-04-11 00:21:24 +0200

    More base implementation for redux

--- src/screens/home-screen/HomeScreen.tsx
+++ src/screens/home-screen/HomeScreen.tsx
@@ -1,14 +1,22 @@
 import React from "react";
 import { HomeScreenClasses } from "./index";
 import { ClassNameMap } from "@material-ui/core/styles/withStyles";
+import { useAppSelector, useAppDispatch } from "../../redux/hooks/hooks";
+import { decrement, increment } from "../../redux/reducers/example/exampleReducer";
 
 interface Props {
   styles: ClassNameMap<HomeScreenClasses>;
 }
 
 const HomeScreen: React.FC<Props> = ({ styles }): JSX.Element => {
+  const count = useAppSelector((state) => state);
+  const dispatch = useAppDispatch();
+
   return (
     <div className={styles.pageWrapperStyles}>
+      {count.counter.value}
+      <button onClick={() => dispatch(increment())}>INCREMENT</button>
+      <button onClick={() => dispatch(decrement())}>DECREMENT</button>
       <p>Home Screen!</p>
     </div>
   );

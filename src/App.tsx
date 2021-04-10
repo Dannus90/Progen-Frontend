78524631a0bab340f2a8a4608bfb056991dda68a
Daniel Persson <persson.daniel.1990@gmail.com>
2021-04-10 23:29:26 +0200

    Begin adding redux

--- src/App.tsx
+++ src/App.tsx
@@ -6,18 +6,22 @@ import { CssBaseline } from "@material-ui/core";
 import { I18nextProvider } from "react-i18next";
 import { ApolloProvider } from "@apollo/client";
 import { apolloClient } from "./ApolloClient";
+import { Provider as ReduxProvider } from "react-redux";
 import i18next from "./i18NextSetup";
+import { store } from "./redux/store";
 
 const App = (): JSX.Element => {
   return (
-    <ThemeProvider theme={mainTheme}>
-      <CssBaseline />
-      <I18nextProvider i18n={i18next}>
-        <ApolloProvider client={apolloClient}>
-          <ApplicationRoutes />
-        </ApolloProvider>
-      </I18nextProvider>
-    </ThemeProvider>
+    <ReduxProvider store={store}>
+      <ThemeProvider theme={mainTheme}>
+        <CssBaseline />
+        <I18nextProvider i18n={i18next}>
+          <ApolloProvider client={apolloClient}>
+            <ApplicationRoutes />
+          </ApolloProvider>
+        </I18nextProvider>
+      </ThemeProvider>
+    </ReduxProvider>
   );
 };
 

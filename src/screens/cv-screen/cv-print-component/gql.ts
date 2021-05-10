26e831eeb55db38f3f929e4ddf873b730ae5b3da
Daniel Persson <persson.daniel.1990@gmail.com>
2021-05-10 22:14:56 +0200

    Add full cv information query to the frontend

---
+++ src/screens/cv-screen/cv-print-component/gql.ts
@@ -0,0 +1,67 @@
+import { gql } from "@apollo/client";
+
+export const GET_FULL_CV_INFORMATION = gql`
+  query Query {
+    fullCvInformation {
+      getFullCvInformation {
+        fullUserInformation {
+          id
+          firstName
+          lastName
+          phoneNumber
+          emailCv
+          citySv
+          cityEn
+          countrySv
+          countryEn
+          profileImage
+        }
+        otherInformation {
+          drivingLicenseSv
+          drivingLicenseEn
+        }
+        educations {
+          educationNameSv
+          educationNameEn
+          examNameSv
+          examNameEn
+          subjectAreaSv
+          subjectAreaEn
+          descriptionSv
+          descriptionEn
+          grade
+          citySv
+          cityEn
+          countryEn
+          countrySv
+          dateStarted
+          dateEnded
+        }
+        workExperiences {
+          employmentRate
+          companyName
+          roleSv
+          roleEn
+          descriptionSv
+          descriptionEn
+          citySv
+          cityEn
+          countrySv
+          countryEn
+          dateStarted
+          dateEnded
+        }
+        languages {
+          languageSv
+          languageEn
+        }
+        userPresentation {
+          presentationSv
+          presentationEn
+        }
+      }
+    }
+  }
+`;
+
+
